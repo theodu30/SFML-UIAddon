3af6682259092e315d6a2ae9469607db37155cdf
theodu30_ <121396182+theodu30@users.noreply.github.com>
2025-10-21 22:49:53 +0200

    Removed nodiscard tag - Size Setter and Getters

--- SFML-UIAddon/SFML-UIAddon-Dll/Code/Headers/SFMLUIAddon/UIPanel.hpp
+++ SFML-UIAddon/SFML-UIAddon-Dll/Code/Headers/SFMLUIAddon/UIPanel.hpp
@@ -2,6 +2,7 @@
 
 #include "Exports.hpp"
 #include <SFML/System/Vector2.hpp>
+#include <SFML/Graphics/RenderTexture.hpp>
 
 namespace sfui
 {
@@ -12,9 +13,16 @@ namespace sfui
 		~UIPanel();
 
 		void setActive(bool _active);
-		[[nodiscard]] bool isActive() const noexcept;
+		bool isActive() const noexcept;
+
+		void setSize(const sf::Vector2u& _size);
+		sf::Vector2u getSize() const noexcept;
+		unsigned int getWidth() const noexcept;
+		unsigned int getHeight() const noexcept;
 	private:
 		bool m_active = false;
 		sf::Vector2u m_size;
+
+		sf::RenderTexture m_renderTexture;
 	};
 }
--- SFML-UIAddon/SFML-UIAddon-Dll/Code/UIPanel.cpp
+++ SFML-UIAddon/SFML-UIAddon-Dll/Code/UIPanel.cpp
@@ -4,6 +4,7 @@ namespace sfui
 {
 	UIPanel::UIPanel()
 	{
+		m_renderTexture = sf::RenderTexture(sf::Vector2u(100, 100));
 	}
 
 	UIPanel::~UIPanel()
@@ -18,5 +19,25 @@ namespace sfui
 	bool UIPanel::isActive() const noexcept
 	{
 		return m_active;
+	}
+
+	void UIPanel::setSize(const sf::Vector2u& _size)
+	{
+		m_size = _size;
+	}
+
+	sf::Vector2u UIPanel::getSize() const noexcept
+	{
+		return m_size;
+	}
+
+	unsigned int UIPanel::getWidth() const noexcept
+	{
+		return m_size.x;
+	}
+
+	unsigned int UIPanel::getHeight() const noexcept
+	{
+		return m_size.y;
 	}
 }
