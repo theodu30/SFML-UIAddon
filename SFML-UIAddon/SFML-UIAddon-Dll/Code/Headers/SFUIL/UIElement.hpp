469de60b4df40356f2732efa4826ab770e6459f0
Arca Théo <121396182+theodu30@users.noreply.github.com>
2025-10-27 14:09:59 +0100

    UIElementSubClass Template - Queries - Children and Parent
    +Added UIElement vector for element children
    +Added UIELement parent of element
    +Added functions to add child to element, detach from parent and remove itself and children from hierarchy
    +Added UIElementSubClass concept template to achieve search and verifications
    +Added query(+- name) and queryAll(+- name) for depth-first search in an element hierarchy

--- SFML-UIAddon/SFML-UIAddon-Dll/Code/Headers/SFUIL/UIElement.hpp
+++ SFML-UIAddon/SFML-UIAddon-Dll/Code/Headers/SFUIL/UIElement.hpp
@@ -3,15 +3,35 @@
 #include "System/Exports.hpp"
 #include "System/UIElementProperty.hpp"
 #include <SFML/Graphics/RenderTexture.hpp>
+#include <vector>
+#include <concepts>
 
 namespace sfui
 {
+	// Template concept to ensure T is derived from UIElement
+	template<typename T>
+	concept UIElementSubClass = std::derived_from<UIElement, T>;
+
+	/// <summary>
+	/// <paragraph>Base class for all UI Elements in the SFML-UIAddon library.</paragraph>
+	/// </summary>
 	class SFUIL_API UIElement
 	{
 	public:
 		UIElement() = default;
+		UIElement(const char* _name) : m_name(_name) {}
 		virtual ~UIElement() = default;
+
+		/// <summary>
+		/// <paragraph>Renders the UI Element to the provided RenderTexture.</paragraph>
+		/// </summary>
+		/// <param name="_texture">The RenderTexture to render to.</param>
 		virtual void render(sf::RenderTexture& _texture) = 0;
+
+		/// <summary>
+		/// <paragraph>Draws the UI Element to the provided RenderTarget.</paragraph>
+		/// </summary>
+		/// <param name="_target">The RenderTarget to draw to.</param>
 		virtual void drawToTarget(sf::RenderTarget& _target) = 0;
 
 		/// <summary>
@@ -80,7 +100,163 @@ namespace sfui
 		/// <returns></returns>
 		DisplayProperty& getDisplayProperty();
 
+		/// <summary>
+		/// <paragraph>Sets the name of the UI Element.</paragraph>
+		/// </summary>
+		/// <param name="_name">The name to set for the UI Element.</param>
+		void setName(const char* _name);
+
+		/// <summary>
+		/// <paragraph>Gets the name of the UI Element.</paragraph>
+		/// </summary>
+		/// <returns></returns>
+		const char* getName() const;
+
+		/// <summary>
+		/// <paragraph>Adds a child UI Element to this element.</paragraph>
+		/// </summary>
+		/// <param name="_child">Pointer to the child UI Element to add.</param>
+		/// <returns>True if the child was added successfully, false otherwise.</returns>
+		bool addChild(UIElement* _child);
+
+		/// <summary>
+		/// <paragraph>Removes itself from its parent UI Element.</paragraph>
+		/// </summary>
+		/// <returns>True if successfully removed from parent, false otherwise.</returns>
+		bool removeFromParent();
+
+		/// <summary>
+		/// <paragraph>Removes itself and all its children from the UI hierarchy.</paragraph>
+		/// <paragraph>Destroys itself all child UI Elements recursively.</paragraph>
+		/// </summary>
+		/// <returns>Will always return true right after successful removal.</returns>
+		bool removeFromHierarchy();
+
+		/// <summary>
+		/// <paragraph>Cast the UI Element to a specific derived type from UIElement.</paragraph>
+		/// </summary>
+		/// <typeparam name="T">The target derived type to cast to.</typeparam>
+		/// <returns>Pointer to the casted type if successful, nullptr otherwise.</returns>
+		template<UIElementSubClass T>
+		T* as()
+		{
+			return dynamic_cast<T*>(this);
+		}
+
+		/// <summary>
+		/// <paragraph>Const version: Cast the UI Element to a specific derived type from UIElement.</paragraph>
+		/// </summary>
+		/// <typeparam name="T">The target derived type to cast to.</typeparam>
+		/// <returns>Pointer to the casted type if successful, nullptr otherwise.</returns>
+		template<UIElementSubClass T>
+		T* as() const
+		{
+			return dynamic_cast<T*>(this);
+		}
+
+		/// <summary>
+		/// <paragraph>Queries the UI Element hierarchy for a descendant of a specific derived type from UIElement.</paragraph>
+		/// <paragraph>Will return the first matching descendant found in a depth-first search manner.</paragraph>
+		/// </summary>
+		/// <typeparam name="T">The target derived type to search for.</typeparam>
+		/// <returns>Pointer to the found descendant if successful, nullptr otherwise.</returns>
+		template<UIElementSubClass T>
+		T* query()
+		{
+			for (UIElement* child : m_children)
+			{
+				if (T* castedChild = child->as<T>())
+				{
+					return castedChild;
+				}
+				if (T* descendant = child->query<T>())
+				{
+					return descendant;
+				}
+			}
+			return nullptr;
+		}
+
+		/// <summary>
+		/// <paragraph>Queries the UI Element hierarchy for a descendant of a specific derived type from UIElement with a specific name.</paragraph>
+		/// <paragraph>Will return the first matching descendant found in a depth-first search manner.</paragraph>
+		/// </summary>
+		/// <typeparam name="T">The target derived type to search for.</typeparam>
+		/// <param name="_name">The name of the UI Element to search for.</param>
+		/// <returns>Pointer to the found descendant if successful, nullptr otherwise.</returns>
+		template <UIElementSubClass T>
+		T* query(const char* _name)
+		{
+			for (UIElement* child : m_children)
+			{
+				if (T* castedChild = child->as<T>())
+				{
+					if (std::strcmp(castedChild->getName(), _name) == 0)
+					{
+						return castedChild;
+					}
+				}
+				if (T* descendant = child->query<T>(_name))
+				{
+					return descendant;
+				}
+			}
+			return nullptr;
+		}
+
+		/// <summary>
+		/// <paragraph>Queries the UI Element hierarchy for all descendants of a specific derived type from UIElement.</paragraph>
+		/// <paragraph>Will return all matching descendants found in a depth-first search manner.</paragraph>
+		/// </summary>
+		/// <typeparam name="T">The target derived type to search for.</typeparam>
+		/// <returns>Vector of pointers to the found descendants.</returns>
+		template <UIElementSubClass T>
+		std::vector<T*> queryAll()
+		{
+			std::vector<T*> results;
+			for (UIElement* child : m_children)
+			{
+				if (T* castedChild = child->as<T>())
+				{
+					results.push_back(castedChild);
+				}
+				std::vector<T*> descendantResults = child->queryAll<T>();
+				results.insert(results.end(), descendantResults.begin(), descendantResults.end());
+			}
+			return results;
+		}
+
+		/// <summary>
+		/// <paragraph>Queries the UI Element hierarchy for all descendants of a specific derived type from UIElement with a specific name.</paragraph>
+		/// <paragraph>Will return all matching descendants found in a depth-first search manner.</paragraph>
+		/// </summary>
+		/// <typeparam name="T">The target derived type to search for.</typeparam>
+		/// <param name="_name">The name of the UI Element to search for.</param>
+		/// <returns>Vector of pointers to the found descendants.</returns>
+		template <UIElementSubClass T>
+		std::vector<T*> queryAll(const char* _name)
+		{
+			std::vector<T*> results;
+			for (UIElement* child : m_children)
+			{
+				if (T* castedChild = child->as<T>())
+				{
+					if (std::strcmp(castedChild->getName(), _name) == 0)
+					{
+						results.push_back(castedChild);
+					}
+				}
+				std::vector<T*> descendantResults = child->queryAll<T>(_name);
+				results.insert(results.end(), descendantResults.begin(), descendantResults.end());
+			}
+			return results;
+		}
+
 	protected:
+		const char* m_name = "UIElement"; // Name of the UI Element
+		std::vector<UIElement*> m_children; // Child UI Elements
+		UIElement* m_parent = nullptr; // Parent UI Element
+
 		SpacingProperty m_spacing;
 		BorderProperty m_border;
 		TransformProperty m_transform;
--- SFML-UIAddon/SFML-UIAddon-Dll/Code/UIElement.cpp
+++ SFML-UIAddon/SFML-UIAddon-Dll/Code/UIElement.cpp
@@ -51,4 +51,59 @@ namespace sfui
 	{
 		return m_display;
 	}
+
+	void UIElement::setName(const char* _name)
+	{
+		m_name = _name;
+	}
+
+	const char* UIElement::getName() const
+	{
+		return m_name;
+	}
+
+	bool UIElement::addChild(UIElement* _child)
+	{
+		if (_child == nullptr)
+		{
+			return false;
+		}
+		_child->m_parent = this;
+		m_children.push_back(_child);
+		return true;
+	}
+
+	bool UIElement::removeFromParent()
+	{
+		if (m_parent == nullptr)
+		{
+			return false;
+		}
+		auto& siblings = m_parent->m_children;
+		auto it = std::find(siblings.begin(), siblings.end(), this);
+		if (it != siblings.end())
+		{
+			siblings.erase(it);
+			m_parent = nullptr;
+			return true;
+		}
+		return false;
+	}
+
+	bool UIElement::removeFromHierarchy()
+	{
+		// Remove from parent first
+		removeFromParent();
+		// Recursively remove all children
+		for (UIElement* child : m_children)
+		{
+			if (child != nullptr)
+			{
+				child->removeFromHierarchy();
+				delete child; // Assuming ownership and responsibility for deletion
+			}
+		}
+		m_children.clear();
+		return true;
+	}
 }
